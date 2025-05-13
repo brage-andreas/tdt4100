fa75c8cda04950a030151342a22f9c2745b9227f
brage-andreas <bragerino+github@gmail.com>
2025-05-13 21:24:10 +0200

    chore: add exams

---
+++ exams/2024-1/eksamen/src/main/java/com/bmw/manufacturing/part2/MainOffice.java
@@ -0,0 +1,18 @@
+package com.bmw.manufacturing.part2;
+
+/**
+ * Implement the class MainOffice.
+ * 
+ * The class must implement the interface {@link no.ntnu.tdt4100.part2.IMainOffice}. 
+ * The class needs no constructor.
+ * 
+ * Read the documentation in the {@link no.ntnu.tdt4100.part2.IMainOffice} interface 
+ * for detailed description on expected behaviour 
+ * of the methods you need to implement.
+ * 
+ * @see no.ntnu.tdt4100.part2.IMainOffice
+ * @see MainOfficeTests
+ */
+
+ // TODO Implement the MainOffice class here according to description in JavaDoc
+public class MainOffice {}
---
+++ exams/2024-1/eksamen/src/main/java/com/bmw/manufacturing/part5/WarehouseInventoryManager.java
@@ -0,0 +1,11 @@
+package com.bmw.manufacturing.part5;
+
+/**
+ * WarehouseInventoryManager must implement the {@link no.ntnu.tdt4100.part5.InventoryManager} interface
+ * 
+ * @see no.ntnu.tdt4100.part5.InventoryManager
+ * @see WarehouseInventoryManagerTests
+ */
+public class WarehouseInventoryManager {
+    // TODO Implement the class according to behaviour specified in Javadoc in its interface
+}
